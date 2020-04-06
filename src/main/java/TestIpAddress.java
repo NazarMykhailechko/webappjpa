847ca8a8bd39e0f25c2bcdd055e6e5c3dd141e78
Nazar <WIN72007@ukr.net>
2020-04-06 22:30:19 +0300

    springbootAppRemote added userlogin

--- src/main/java/TestIpAddress.java
+++ src/main/java/TestIpAddress.java
@@ -1,12 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class TestIpAddress {
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws IOException {
         InetAddress InetAddress = java.net.InetAddress.getLocalHost();
         System.out.println(InetAddress);
         System.out.println(System.getenv("username"));
+
+        Runtime r = Runtime.getRuntime();
+        Process p = r.exec("hostname");
+        BufferedReader rr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        System.out.println(rr.readLine());
     }
 
 }
--- src/main/java/webapplication/controller/ClientController.java
+++ src/main/java/webapplication/controller/ClientController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import webapplication.dao.ClientDao;
 import webapplication.model.Client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -35,11 +38,17 @@ public class ClientController {
     }
 
     @RequestMapping("/")
-    public String home(Model model) throws UnknownHostException {
+    public String home(Model model) throws IOException {
         String userlogin = System.getenv("username");
         InetAddress InetAddress = java.net.InetAddress.getLocalHost();
+
+        Runtime r = Runtime.getRuntime();
+        Process p = r.exec("hostname");
+        BufferedReader rr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        System.out.println(rr.readLine());
+
         model.addAttribute("listOfClients", clientDao.findByUserlogin(userlogin));
-        model.addAttribute("userlogin", InetAddress);
+        model.addAttribute("userlogin", rr.readLine());
 
 //        for (Client cl : clientDao.findByUserlogin(System.getenv("username"))) {
 //            System.out.print(cl.toString());
