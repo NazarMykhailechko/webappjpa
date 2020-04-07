d85ceab042328bf87ddf1bc30e4e6844a502b59c
Nazar <WIN72007@ukr.net>
2020-04-07 10:23:55 +0300

    springbootAppRemote added userlogin

--- src/main/java/webapplication/controller/ClientController.java
+++ src/main/java/webapplication/controller/ClientController.java
@@ -10,14 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import webapplication.dao.ClientDao;
 import webapplication.model.Client;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.security.Principal;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/")
@@ -45,19 +38,19 @@ public class ClientController {
     }
 
     @RequestMapping("/")
-    public String home(Model model, HttpServletRequest request, Principal principal) throws IOException {
-        System.out.println(request.getRemoteAddr());
-        String userlogin = System.getenv("username");
-        Map<String, String> userlogin1 = System.getenv();
-        InetAddress InetAddress = java.net.InetAddress.getLoopbackAddress();
-        String sysss = System.getProperty("user.name");
-        Runtime r = Runtime.getRuntime();
-        Process p = r.exec("whoami");
-        BufferedReader rr = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        System.out.println(rr.readLine());
-        String remoteAddr = request.getHeader("REMOTE_ADDR");
-        model.addAttribute("listOfClients", clientDao.findByUserlogin(userlogin));
-        model.addAttribute("userlogin", remoteAddr);
+    public String home(Model model) throws IOException {
+//        System.out.println(request.getRemoteAddr());
+//        String userlogin = System.getenv("username");
+//        Map<String, String> userlogin1 = System.getenv();
+//        InetAddress InetAddress = java.net.InetAddress.getLoopbackAddress();
+//        String sysss = System.getProperty("user.name");
+//        Runtime r = Runtime.getRuntime();
+//        Process p = r.exec("whoami");
+//        BufferedReader rr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//        System.out.println(rr.readLine());
+//        String remoteAddr = request.getHeader("REMOTE_ADDR");
+//        model.addAttribute("userlogin", remoteAddr);
+        model.addAttribute("listOfClients", clientDao.findAll());
 
 //        for (Client cl : clientDao.findByUserlogin(System.getenv("username"))) {
 //            System.out.print(cl.toString());
