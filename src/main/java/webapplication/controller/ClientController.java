35b8494056b081f54e841e47bc75d6b440f68a11
Nazar <WIN72007@ukr.net>
2020-04-07 09:47:41 +0300

    springbootAppRemote added userlogin

--- src/main/java/webapplication/controller/ClientController.java
+++ src/main/java/webapplication/controller/ClientController.java
@@ -55,7 +55,7 @@ public class ClientController {
         Process p = r.exec("whoami");
         BufferedReader rr = new BufferedReader(new InputStreamReader(p.getInputStream()));
         System.out.println(rr.readLine());
-        String remoteAddr = request.getHeader("HTTP_FORWARDED_FOR");
+        String remoteAddr = request.getHeader("HTTP_FORWARDED");
         model.addAttribute("listOfClients", clientDao.findByUserlogin(userlogin));
         model.addAttribute("userlogin", remoteAddr);
 
